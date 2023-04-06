a3f0357cac8e63876b1dccf9b89bb971290f31e6
Eric Frey <ericlfrey@gmail.com>
2023-04-06 14:03:27 -0500

    Finished Chapter 5. PUT Requests work for all nodes.

--- views/customer_requests.py
+++ views/customer_requests.py
@@ -47,3 +47,11 @@ def delete_customer(id):
 
     if customer_index >= 0:
         CUSTOMERS.pop(customer_index)
+
+
+def update_customer(id, new_customer):
+    """Updates customer with Replacement"""
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            CUSTOMERS[index] = new_customer
+            break
